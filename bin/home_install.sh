c54b52ef37dc31c5dbd54546f3186085505882ef
Chris Evich <chris-yewess@anonomail.me>
2020-06-27 09:17:26 -0400

    Modernize/simplify installer
    Signed-off-by: Chris Evich <chris-yewess@anonomail.me>

--- bin/home_install.sh
+++ bin/home_install.sh
@@ -1,34 +1,77 @@
 #!/bin/bash
 
-DOTFILES="astylerc bash_profile bashrc gitconfig vimrc"
-DIRS="bin"
+set -eo pipefail
 
-if ! [ -e "${PWD}/README.md" ] || ! [ -d "${PWD}/.git" ]
-then
-    echo "Script must be run from repository root"
+REPO_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}")/../)
+# Files to simply place into $HOME with a '.' prefix (backing up existing)
+DOTFILES="astylerc bash_profile bashrc gitconfig vimrc screenrc tmux.conf"
+# Directories to recursively link files into $HOME
+DIRS="bin .gnupg .config"
+
+msg() { echo "${1:-No message specified!}" &> /dev/stderr; }
+
+if [[ ! -r "${REPO_ROOT}/README.md" ]] || [[ ! -d "${REPO_ROOT}/.git" ]]; then
+    msg "Error: Expecting to find the repository root at '$REPO_ROOT'"
     exit 1
 fi
 
-for DOTFILE in $DOTFILES
-do
-    if ! [ -e "${PWD}/${DOTFILE}" ]
-    then
-        echo "$DOTFILE not found: Script must be run from repository root"
-        exit 2
+# Always return the full absolute dereferenced path to a possibly missing file
+truepath() {
+    local filepath="$1"
+    if [[ -z "$filepath" ]]; then
+        msg "Error: Expecting a non-empty filepath as the first argument"
+        exit 7
     fi
-    rm -f "${HOME}/.${DOTFILE}"
-    ln -s "$(basename $PWD)/${DOTFILE}" "${HOME}/.${DOTFILE}"
-done
 
+    local truefilepath="$filepath"  # Couldn't be resolved for any reason
+    # Any bad links/missing files will fallback to the above
+    local linkedfile=$(readlink --canonicalize-missing "$filepath" 2>/dev/null)  # empty when not a link
+    if [[ -n "$linkedfile" ]]; then
+        truefilepath=$(realpath "$linkedfile" 2>/dev/null)  # link may be relative?
+    fi
+    echo "$truefilepath"
+}
+
+install_link(){
+    local src=$(truepath "$1")
+    local srcname=$(basename "$src")
+    local dst="$2"
+    local dstdir=$(dirname "$dst")
+
+    if [[ ! -r "$src" ]]; then
+        msg "Error: The source '$src' file not found"
+        exit 2
+    elif [[ -d "$dst" ]]; then
+        msg "Error: Refusing to install as a directory"
+        exit 3
+    elif [[ -z "$src" ]] || [[ -z "$dst" ]]; then
+        msg "Error Refusing to install an empty source '$src' or destination '$dst'"
+        exit 4
+    elif [[ "$srcname" =~ "$(basename $0)" ]]; then
+        msg "Skipping installing the installer-script or similar file"
+        return 0
+    elif [[ "$src" =~ "~*~$" ]] || [[ "$dst" =~ "~*~$" ]]; then
+        msg "Error: Encountered a backup source '$src' or destination '$dst'"
+        exit 5
+    elif [[ "$src" == $(truepath "$dst") ]]; then
+        msg "Skipping install of duplicate link to '"$(truepath "$src")"'"
+        return 0
+    elif [[ ! -d "$dstdir" ]]; then
+        mkdir -vp "$dstdir"
+    fi
+    echo -n "Installing link from " &> /dev/stderr
+    ln --symbolic --verbose --backup=numbered "$src" "$dst"
+}
+
+cd $REPO_ROOT
+msg "Installing configuration and Script files"
 for DIR in $DIRS
 do
-    if [ -e "${HOME}/${DIR}" ]
-    then
-        mv "${HOME}/${DIR}" "${HOME}/${DIR}.original"
-    fi
-    ln -s "$(basename $PWD)/${DIR}" "${HOME}/${DIR}"
+    for relative_filepath in $(find $DIR -type f); do
+        install_link $REPO_ROOT/$relative_filepath $HOME/$relative_filepath
+    done
 done
 
-# Install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+for DOTFILE in $DOTFILES; do
+    install_link "$REPO_ROOT/$DOTFILE" "$HOME/.$DOTFILE"
+done
